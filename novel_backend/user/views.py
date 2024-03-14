e2f41e7f1371af3de6bd49c065194ea3e1c9b18f
HTHTHTHTHTtttt <954569093@qq.com>
2024-03-14 15:10:06 +0800

    fix register and login

--- novel_backend/user/views.py
+++ novel_backend/user/views.py
@@ -1,10 +1,10 @@
 
 import re
 from rest_framework import viewsets
-from .models import NUser,User
+from .models import NUser
 from .serializers import NUserSerializer
 from rest_framework.response import Response
-from django.contrib.auth import authenticate
+# from django.contrib.auth import authenticate
 
 class NUserViewSet(viewsets.ModelViewSet):
     queryset = NUser.objects.all()
@@ -17,13 +17,14 @@ class NUserViewSet(viewsets.ModelViewSet):
         if not re.match(r'^[a-z0-9][\w\.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}){1,2}$', email):
             msg = '邮箱错误'
             return Response(data={"detail": "邮箱格式错误"}, status=404)
-        User.objects.create_user(username=username,password=password,email=email)
+        users=NUser.objects.create(username=username,password=password,email=email)
+        users.save()
         return Response(data={"detail":"success"})
 
     def login(self,request):
         username = request.data.get('username')
         password = request.data.get('password')
-        users=authenticate(request,username=username,password=password)
+        users=NUser.objects.filter(username=username,password=password)
         if not users:
             return Response(data={"detail": " 用户名或密码不正确 "}, status=404)
         return Response(data={"detail":"success"})

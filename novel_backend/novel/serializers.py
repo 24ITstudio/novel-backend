530d3121cac64e37d432263fb0e037503fc59966
HTHTHTHTHTtttt <954569093@qq.com>
2024-03-18 14:39:32 +0800

    add chapters to a novel

--- novel_backend/novel/serializers.py
+++ novel_backend/novel/serializers.py
@@ -1,9 +1,14 @@
 
 from rest_framework import serializers
-from .models import Novel
+from .models import Novel,Chapter
 
 
 class NovelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Novel
+        fields = "__all__"
+
+class ChapterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Chapter
         fields = "__all__"
